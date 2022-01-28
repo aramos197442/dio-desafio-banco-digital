a9d71079ba1a9953477a718a072de7658ce616f9
aramos197442 <aramos197442@gmail.com>
2022-01-28 19:49:30 -0300

    Implementações minhas para o desafio

--- src/Conta.java
+++ src/Conta.java
@@ -1,15 +1,21 @@
 
 public abstract class Conta implements IConta {
 	
+
+	private static final int BANCO_PADRAO = 260;
 	private static final int AGENCIA_PADRAO = 1;
 	private static int SEQUENCIAL = 1;
+	private static final double CPMF = 0.0025d;
 
+	protected int banco;
 	protected int agencia;
 	protected int numero;
 	protected double saldo;
+	protected double cpmf;
 	protected Cliente cliente;
 
 	public Conta(Cliente cliente) {
+		this.banco = Conta.BANCO_PADRAO;
 		this.agencia = Conta.AGENCIA_PADRAO;
 		this.numero = SEQUENCIAL++;
 		this.cliente = cliente;
@@ -17,18 +23,30 @@ public abstract class Conta implements IConta {
 
 	@Override
 	public void sacar(double valor) {
-		saldo -= valor;
+		this.saldo -= valor;
 	}
 
 	@Override
 	public void depositar(double valor) {
-		saldo += valor;
+		this.saldo += valor;
 	}
 
 	@Override
-	public void transferir(double valor, IConta contaDestino) {
+	public void transferir(double valor, Conta contaDestino) {
 		this.sacar(valor);
-		contaDestino.depositar(valor);
+		
+//	    Verifica a Titularidade da conta de destino e caso sejam diferentes há a cobrança de CPMF na conta originária da transação.
+//      OBS. Verificação por nome dps Titulares para simplificação do exercício.		
+		if (this.cliente.getNome() != contaDestino.cliente.getNome()) {
+			contaDestino.depositar(valor);
+			this.sacar(valor*CPMF);
+			this.cpmf += (valor*CPMF);
+		}
+		else contaDestino.depositar(valor);
+	}
+
+	public int getBanco() {
+		return banco;
 	}
 
 	public int getAgencia() {
@@ -45,8 +63,10 @@ public abstract class Conta implements IConta {
 
 	protected void imprimirInfosComuns() {
 		System.out.println(String.format("Titular: %s", this.cliente.getNome()));
+		System.out.println(String.format("Banco: %d", this.banco));
 		System.out.println(String.format("Agencia: %d", this.agencia));
 		System.out.println(String.format("Numero: %d", this.numero));
 		System.out.println(String.format("Saldo: %.2f", this.saldo));
+		System.out.println(String.format("CPMF: (%.2f)", this.cpmf));
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,58 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+/*
+ *      Cria cliente Alexandre
+ */
+		Cliente cliente = new Cliente();
+		cliente.setNome("Alexandre");
 
-		cc.depositar(100);
+/*
+ *      Cria cliente Cristiane		
+ */
+		Cliente outroCliente = new Cliente();
+		outroCliente.setNome("Cristiane");
+		
+/*
+ *      Cria contas CC e Poupança para Alexandre
+ */
+		Conta cc = new ContaCorrente(cliente);
+		Conta poupanca = new ContaPoupanca(cliente);
+		
+/*
+*       Cria conta CC Cristiane
+*/
+		Conta outraCC = new ContaCorrente(outroCliente);
+		
+
+/*
+ *      Deposita $ 1.000,00 para CC de Alexandre e imprime um extrato		
+ */
+		cc.depositar(1000);
+		cc.imprimirExtrato();
+		
+/*
+ *      Transfere $100,00 para conta Poupança de mesma titularidade
+ */
 		cc.transferir(100, poupanca);
 		
+/*
+ *      Transfere $200,00 para CC de outra titularidade ocasionando cobrança de CPMF
+ */
+		cc.transferir(200, outraCC);
+		
+/*
+ *      Imprime extratos de todas as contas
+ */
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
+		
+		outraCC.imprimirExtrato();
 	}
-
 }
+
+/*
+ * OBS. FINAIS: O ideal seria a verificação de Titularidade por um documento oficial mas para simplificação foi usado 
+ * o Nome do Titular.
+ * 
+ */
